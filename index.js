ede2a4d1f3a7c14d4e827bc062a0bd039e505852
stevieyu <yzm629@sina.cn>
2024-06-26 18:48:38 +0800

    -

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-// run `node index.js` in the terminal
+import {parseYAML} from "confbox";
+import {readFileSync} from 'fs'
+import { gql, Client, cacheExchange, fetchExchange } from '@urql/core';
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const graphqlConfig = parseYAML(readFileSync(process.cwd() + '/.graphqlrc.yml')).schema[0]
+
+const client = new Client({
+  url: Object.keys(graphqlConfig)[0],
+  exchanges: [cacheExchange, fetchExchange],
+  fetchOptions: () => {
+    return Object.values(graphqlConfig)[0];
+  },
+});
+
+const query = gql`
+  query {
+    categories {
+      id
+      name
+      pid
+    }
+  }
+`;
+const variables = {}
+
+await client
+  .query(query, variables)
+  .toPromise()
+  .then(({data}) => {
+    console.log(data);
+  });
+
+// console.log(res);
+
